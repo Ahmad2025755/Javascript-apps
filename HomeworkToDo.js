23ef532cd547d0dd6c21bc113737f907f765e4da
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-11-19 17:33:38 +0500

    Add files via upload

---
+++ HomeworkToDo.js
@@ -0,0 +1,23 @@
+var myForm = document.getElementById("myform")
+var myInput = document.getElementById("myInput")
+var myItem = document.getElementById("myItem")
+
+myForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    createItem(myInput.value);
+});
+
+function createItem(inputItems) {
+
+    var things = `<li>${inputItems}<button onclick = "deleteElement(this)">Delete</button></li>`
+    myItem.insertAdjacentHTML("beforeend", things);
+
+myInput.value = "";
+myInput.focus();
+
+}
+
+function deleteElement(ElementToDelete) {
+    ElementToDelete.parentElement.remove();
+}
+ 
