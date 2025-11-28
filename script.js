b7a2e64123e34e13a5ec50b7ba8bd5d08fda379a
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-11-28 16:02:41 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,33 @@
-window.onload = function() {
-    
-    var seconds = 0
-    var milliseconds = 0
+var mydice;
+var dices = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861;'];
+var stopped = true;
+var t;
 
 
-    var appendMilliseconds = document.getElementById("milliseconds");
-    var appendSeconds = document.getElementById("seconds");
+function change() {
 
-    var buttonStart = document.getElementById('Button-start');
-    var buttonStop = document.getElementById('Button-stop');
-    var buttonReset = document.getElementById('Button-reset');
-
-
-    var Interval;
-
-    buttonStart.onclick = function () {
-        clearInterval(Interval);
-        Interval = setInterval(startTimer, 10);
-    }
-
-    buttonStop.onclick = function () {
-        clearInterval(Interval);
-    }
-
-    buttonReset.onclick = function () {
-        clearInterval(Interval)
-        milliseconds = "00";
-        seconds = "00";
-        appendMilliseconds.innerHTML = milliseconds;
-        appendSeconds.innerHTML = seconds;
-    }
-
-
-
-function startTimer() {
-    
-    milliseconds++;
-
-    if(milliseconds <= 9) {
-        appendMilliseconds.innerHTML = "0" + milliseconds;
-    }
-
-    if(milliseconds > 9) {
-        appendMilliseconds.innerHTML =  milliseconds;
-    }
-
-    if(milliseconds > 99) {
-        seconds++;
-        appendSeconds.innerHTML = "0" + seconds;
-        milliseconds = 0;
-        appendMilliseconds.innerHTML = "0" + 0;
-    }
-
-    if(seconds > 9) {
-        appendSeconds.innerHTML = "0" + seconds;
-    }
-
+    var random = Math.floor(Math.random() * 6);
+    mydice.innerHTML = dices[random];
 }
-}
+
+
+function stopstart() {
+
+    if (stopped) {
+        stopped = false;
+
+        t = setInterval(change,1000);
+
+    } else {
+        clearInterval(t);
+        stopped = true;
+    }
+
+};
+
+
+
+window.onload = function () {
+        mydice = document.getElementById("dice");
+        stopstart();
+    }
